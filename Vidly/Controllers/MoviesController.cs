a949ee0a550239519cca3bb6a8f307eebf40df0e
iWithman <witman2@yahoo.com>
2020-03-09 23:55:58 -0400

    Created a list of customers, a NavBar partial view, and also bootstrap layout. I also config some routes to match the path in the url.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,16 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer: Withman"},
+                new Customer { Name = "Customer: iWithman"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customer = customers
+            };
+            return View(viewModel);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
---
+++ Vidly/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customer { get; set; }
+    }
+}
